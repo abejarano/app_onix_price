58cf1dd3336d41d746965441d1350f340c9ec15d
abejarano <abejarano@pinttosoft.com>
2018-10-07 22:09:27 -0400

    save price btc for country

--- price_onixcoin/management/commands/price_btc_localbitcoin.py
+++ price_onixcoin/management/commands/price_btc_localbitcoin.py
@@ -6,7 +6,7 @@ from decimal import Decimal
 
 import os
 import requests
-from price_onixcoin.models import PriceBtcLocalbitcoin
+from price_onixcoin.models import PriceBtcLocalbitcoin, Country
 
 BASE_DIR = os.path.dirname(os.path.dirname(
     os.path.abspath(__file__))) + '/commands/'
@@ -22,21 +22,26 @@ class Command(BaseCommand):
         resp = r.json()
 
         precio_btc_usd_local = resp['USD']
-        precio_btc_bs_local = resp['VEF']
+
+        # buscar los paises
+        paises = Country.objects.all()
+
         try:
+            for pais in paises:
+                precio_btc_bs_local = resp[pais.code_currency]
+                PriceBtcLocalbitcoin(
+                    price_btc_usd_avg_1h=Decimal(precio_btc_usd_local['rates']['last']),
+                    price_btc_usd_avg_6h=Decimal(precio_btc_usd_local['avg_6h']),
+                    price_btc_usd_avg_12h=Decimal(
+                        precio_btc_usd_local['avg_12h']),
+                    price_btc_usd_avg_24h=Decimal(
+                        precio_btc_usd_local['avg_24h']),
 
-            PriceBtcLocalbitcoin(
-                price_btc_usd_avg_1h=Decimal(precio_btc_usd_local['rates']['last']),
-                price_btc_usd_avg_6h=Decimal(precio_btc_usd_local['avg_6h']),
-                price_btc_usd_avg_12h=Decimal(
-                    precio_btc_usd_local['avg_12h']),
-                price_btc_usd_avg_24h=Decimal(
-                    precio_btc_usd_local['avg_24h']),
-
-                price_btc_bs_avg_1h=Decimal(precio_btc_bs_local['rates']['last']),
-                price_btc_bs_avg_6h=Decimal(precio_btc_bs_local['avg_6h']),
-                price_btc_bs_avg_12h=Decimal(precio_btc_bs_local['avg_12h']),
-                price_btc_bs_avg_24h=Decimal(precio_btc_bs_local['avg_24h']),
-            ).save()
+                    price_btc_bs_avg_1h=Decimal(precio_btc_bs_local['rates']['last']),
+                    price_btc_bs_avg_6h=Decimal(precio_btc_bs_local['avg_6h']),
+                    price_btc_bs_avg_12h=Decimal(precio_btc_bs_local['avg_12h']),
+                    price_btc_bs_avg_24h=Decimal(precio_btc_bs_local['avg_24h']),
+                    country=pais
+                ).save()
         except decimal.InvalidOperation as e:
             pass
--- price_onixcoin/models.py
+++ price_onixcoin/models.py
@@ -1,8 +1,21 @@
 from django.db import models
 from django.utils import timezone
-
+from django.utils.translation import ugettext_lazy as _
 
 # Create your models here.
+
+class Country(models.Model):
+    code_country = models.CharField(max_length=3, primary_key=True)
+    country = models.CharField(max_length=60, null=False, blank=False, verbose_name=_('Páis'))
+    code_currency = models.CharField(max_length=3, null=False, blank=False, verbose_name=_('Simbolo de moneda'))
+
+
+    def __str__(self):
+        return self.country
+
+    class Meta:
+        db_table = 'onx_price_country'
+        verbose_name_plural = _('Paises')
 
 
 class PriceBtcLocalbitcoin(models.Model):
@@ -30,12 +43,14 @@ class PriceBtcLocalbitcoin(models.Model):
     price_btc_bs_avg_24h = models.DecimalField(
         max_digits=20, decimal_places=8, null=False, blank=False
     )
+    country = models.ForeignKey(Country, default='VEN', on_delete=models.PROTECT)
     created_at = models.DateTimeField(default=timezone.now)
 
     class Meta:
         db_table = 'onx_price_btc'
         ordering = ['-created_at']
 
+        verbose_name_plural = _('Precios bitcoins')
 
 class PriceOnix(models.Model):
     btc_onx_buy = models.DecimalField(
@@ -56,13 +71,13 @@ class PriceOnix(models.Model):
     usd_onx_sell = models.DecimalField(
         max_digits=20, decimal_places=8, null=False, blank=False
     )
-
+    country = models.ForeignKey(Country, default='VEN', on_delete=models.PROTECT)
     created_at = models.DateTimeField(default=timezone.now)
 
     class Meta:
         db_table = 'onx_historico_price'
         ordering = ['-created_at']
-
+        verbose_name_plural = _('Precios del onix')
 
 class Yobit(models.Model):
     btc_onx_sell = models.DecimalField(
